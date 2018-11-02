24f34442c7635485bb3959f88ec08e426855d0a3
Marin Maksutaj <marin.maksutaj@ubt.edu.al>
2018-11-02 20:26:45 +0100

    Changed the ajax url attribute to the right server

--- front/init.js
+++ front/init.js
@@ -4,14 +4,14 @@ $(document).ready(function() {
         const originalLink = $("#link_form").val();
         let reqObj = {
         originalUrl: originalLink,
-        shortBaseUrl: "http://localhost:42069"
+        shortBaseUrl: "https://short-y.herokuapp.com"
 		};
 		//alert(dirname__);
         $.ajax({
             type: 'POST',
             data: JSON.stringify(reqObj),
             contentType: 'application/json; charset=utf-8',
-            url: 'http://localhost:42069/api/item',
+            url: 'https://short-y.herokuapp.com/api/item',
             success: function(data) {
             	//alert(JSON.stringify(data));
                 $("#result").html("Short link : <a href=\"" + data.shortUrl + "\"" + ">" +data.shortUrl + "</a>");
